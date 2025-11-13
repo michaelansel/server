5468298ae057dddd90f86788a7539152b0ddf69c
Maxim Raznatovski <nda.mr43@gmail.com>
2025-11-13 12:57:59 +0100

    Fix track progress reporting to use corrected elapsed time

--- music_assistant/providers/resonate/player.py
+++ music_assistant/providers/resonate/player.py
@@ -491,8 +491,8 @@ class ResonatePlayer(Player):
             year=year,
             track=track,
             track_duration=track_duration * 1000 if track_duration is not None else None,
-            track_progress=int(self._attr_elapsed_time * 1000)
-            if self._attr_elapsed_time is not None
+            track_progress=int(self.corrected_elapsed_time * 1000)
+            if self.corrected_elapsed_time is not None
             else 0,
             playback_speed=1000,
             repeat=repeat,
